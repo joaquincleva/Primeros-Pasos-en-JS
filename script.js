f1c6814ccb3c11108aed8ddce2a233eb6283d889
joaquincleva <joaquin.cleva@gmail.com>
2022-05-13 14:39:24 -0300

    Primer Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+/*Se llama a una función llamada a alert para que muestre
+una alerta con el mensaje que le indicamos.*/
+alert("Esto es una alerta de un documento externo.");
+alert("Soy el primer script.");
