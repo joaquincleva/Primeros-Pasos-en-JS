d5a138c5737ef7002c4a40cf5acf57400ebecf59
joaquincleva <joaquin.cleva@gmail.com>
2022-05-13 14:55:42 -0300

    Agragado comentarios

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /*Se llama a una función llamada a alert para que muestre
 una alerta con el mensaje que le indicamos.*/
+/*Primera alerta desde JavaScript*/
 alert("Esto es una alerta de un documento externo.");
+/*Segunda alerta*/
 alert("Soy el primer script.");
